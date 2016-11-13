b3555cb882688e985fa3dee462a090ef1e86ab0a
Shubham Chaudhary <me@shubhamchaudhary.in>
2016-11-13 19:01:15 +0000

    Remove requirement on google-mock from apt-get

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+#git submodule update
 apt-get install -y cmake g++  # add sudo according to taste
 apt-get install -y ctags
-apt-get install -y google-mock
+#apt-get install -y google-mock
