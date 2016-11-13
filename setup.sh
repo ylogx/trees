99e133a6f3eef9ada9f451326058e1b40cf0a4ab
Shubham Chaudhary <me@shubhamchaudhary.in>
2016-11-13 19:38:21 +0000

    Install git before submodule update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-git submodule update
+apt-get install -y git
+git submodule update --init --recursive
 apt-get install -y cmake g++  # add sudo according to taste
 apt-get install -y ctags
 #apt-get install -y google-mock
