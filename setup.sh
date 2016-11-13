6e7e0a4b57e4dacd8a6a92abb16609e216999c4f
Shubham Chaudhary <me@shubhamchaudhary.in>
2016-11-13 19:06:58 +0000

    Run submodule update with setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-#git submodule update
+git submodule update
 apt-get install -y cmake g++  # add sudo according to taste
 apt-get install -y ctags
 #apt-get install -y google-mock
