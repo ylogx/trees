7518252bbd57cf2d52e9bfdf484e96d38bec4381
Shubham Chaudhary <me@shubhamchaudhary.in>
2016-11-14 21:36:22 +0000

    Rename test classes

--- src/node_test.cc
+++ src/node_test.cc
@@ -2,29 +2,31 @@
 #include "../include/traversal.h"
 #include <gtest/gtest.h>
 
-TEST(getNode, MustReturnNonNullNode) {
+/// NodeGetNode
+TEST(NodeGetNode, MustReturnNonNullNode) {
     Node *node = Node::getNode(10, NULL, NULL);
     ASSERT_TRUE(node != NULL);
 }
 
-TEST(getNode, MustHaveCorrectInfo) {
+TEST(NodeGetNode, MustHaveCorrectInfo) {
     Node *node = Node::getNode(10, NULL, NULL);
     ASSERT_EQ(node->info, 10);
 }
 
-TEST(getNode, MustHaveNoFather) {
+TEST(NodeGetNode, MustHaveNoFather) {
     Node *node = Node::getNode(10, NULL, NULL);
     ASSERT_TRUE(node->father == NULL);
 }
 
-TEST(getNode, MustHaveNoSons) {
+TEST(NodeGetNode, MustHaveNoSons) {
     Node *node = Node::getNode(10, NULL, NULL);
     /*ASSERT_EQ(node->left, NULL);*/
     ASSERT_TRUE(node->left == NULL);
     ASSERT_TRUE(node->right == NULL);
 }
 
-TEST(getLeaf, MustHaveNoSons) {
+/// NodeGetLeaf
+TEST(NodeGetLeaf, MustHaveNoSons) {
     Node *node = Node::getLeaf(10);
     ASSERT_EQ(node->info, 10);
     ASSERT_TRUE(node->left == NULL);
@@ -33,18 +35,34 @@ TEST(getLeaf, MustHaveNoSons) {
 }
 
 
-
+/// Node SonsMethods
 Node * getDummyTree() {
     return makeTree();
 }
 
-TEST(sonsMethods, MustSayLeftWhenLeft) {
+TEST(NodeSonsMethods, MustHaveCorrectInfoInSons) {
+    Node *tree = getDummyTree();
+    ASSERT_TRUE(tree->right != NULL);
+    ASSERT_EQ(tree->left->info, 50);
+    ASSERT_TRUE(tree->left != NULL);
+    ASSERT_EQ(tree->right->info, 150);
+}
+
+TEST(NodeSonsMethods, MustHaveCorrectInfoInGrandSons) {
+    Node *tree = getDummyTree();
+    ASSERT_TRUE(tree->right->left != NULL);
+    ASSERT_EQ(tree->right->left->info, 120);
+    ASSERT_TRUE(tree->left->right != NULL);
+    ASSERT_EQ(tree->left->right->info, 75);
+}
+
+TEST(NodeSonsMethods, MustSayLeftWhenLeft) {
     Node *tree = getDummyTree();
     ASSERT_TRUE(tree->left->isLeft());
     ASSERT_FALSE(tree->left->isRight());
 }
 
-TEST(sonsMethods, MustSayRightWhenRight) {
+TEST(NodeSonsMethods, MustSayRightWhenRight) {
     Node *tree = getDummyTree();
     ASSERT_TRUE(tree->right->isRight());
     ASSERT_FALSE(tree->right->isLeft());
